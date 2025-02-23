eff732166fd07ca300185ca58fb7e36acc1ba99b
Arthur <arthur@gdgnantes.com>
2025-02-23 19:51:45 +0100

    youtube no-cookie

--- src/components/commun/youtube.tsx
+++ src/components/commun/youtube.tsx
@@ -1,30 +1,33 @@
-import { Stack, Typography } from "@mui/material";
-import React from "react";
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
 
 export const Youtube: React.FC<{ id: string; title: string }> = ({
   id,
   title,
 }) => (
-  <Stack textAlign="center" justifyContent="center">
-    <Typography variant="h2">{title}</Typography>
+  <Stack textAlign='center' justifyContent='center'>
+    <Typography variant='h2'>{title}</Typography>
     <div
       style={{
-        position: "relative",
-        paddingBottom: "56.25%",
+        position: 'relative',
+        paddingBottom: '56.25%',
         height: 0,
-        overflow: "hidden",
+        overflow: 'hidden',
       }}
     >
       <iframe
-        src={"https://www.youtube.com/embed/" + id}
+        src={'https://www.youtube-nocookie.com/embed/' + id}
         title={title}
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        referrerPolicy='strict-origin-when-cross-origin'
         allowFullScreen
+        frameBorder='0'
         style={{
-          position: "absolute",
+          position: 'absolute',
           top: 0,
           left: 0,
-          width: "100%",
-          height: "100%",
+          width: '100%',
+          height: '100%',
         }}
       />
     </div>
