760e95a60510cb8ae69e1ddf095554e0a3cfc644
Arthur <arthur@gdgnantes.com>
2025-03-04 23:38:57 +0100

    réduction tailles images + fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   siteUrl: 'https://devfest2025.gdgnantes.com',
   generateRobotsTxt: true,
+  transform: (config, path) => {
+    if (!path.startsWith('/fr') && !path.startsWith('/en')) {
+      return {};
+    }
+    return {
+      loc: config.siteUrl + path.replace(/^\/fr/, ''),
+      changefreq: 'daily',
+      priority: 0.7,
+    };
+  },
 };
