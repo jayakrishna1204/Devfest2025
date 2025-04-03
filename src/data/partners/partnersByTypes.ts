11e52d40f19ef1b0a42f5a91f4f0e6e1577d5577
GUITTENY Brian <bguitteny@luccasoftware.com>
2025-04-03 15:22:30 +0200

    images

--- src/data/partners/partnersByTypes.ts
+++ src/data/partners/partnersByTypes.ts
@@ -10,150 +10,102 @@ export type PartnerType = 'Platinium' | 'Gold' | 'Virtual' | 'Velotypie';
 export const partnersByTypes: Record<PartnerType, Partner[]> = {
   Platinium: [
     {
-      id: 'apside',
-      name: 'Apside',
-      image: await import('@/images/partners/apside.png'),
-      website: 'https://www.apside.com'
-    },
-    {
-      id: 'bouygues-telecom',
-      name: 'Bouygues Telecom',
-      image: await import('@/images/partners/bouygues-telecom.png'),
-      website: 'https://www.bouyguestelecom.fr'
-    },
-    {
-      id: 'capgemini',
-      name: 'Capgemini',
-      image: await import('@/images/partners/capgemini.png'),
-      website: 'https://www.capgemini.com'
-    },
-    {
-      id: 'generali',
-      name: 'Generali',
-      image: await import('@/images/partners/generali.png'),
-      website: 'https://www.generali.fr'
-    },
-    {
-      id: 'google',
-      name: 'Google',
-      image: await import('@/images/partners/google.png'),
-      website: 'https://developers.google.com/'
+      id: 'proginov',
+      name: 'Proginov',
+      image: await import('@/images/partners/proginov.png'),
+      website: 'https://www.proginov.com/'
     },
     {
       id: 'onepoint',
       name: 'Onepoint',
       image: await import('@/images/partners/onepoint.png'),
-      website: 'https://www.groupeonepoint.com/fr/'
+      website: 'https://www.groupeonepoint.com'
     },
     {
-      id: 'proginov',
-      name: 'Proginov',
-      image: await import('@/images/partners/proginov.png'),
-      website: 'https://www.proginov.com'
+      id: 'apside',
+      name: 'Apside',
+      image: await import('@/images/partners/apside.png'),
+      website: 'https://www.apside.com/'
     },
     {
-      id: 'sfeir',
-      name: 'SFEIR',
-      image: await import('@/images/partners/sfeir.png'),
-      website: 'https://www.sfeir.com'
+      id: 'u-tech',
+      name: 'U-Tech',
+      image: await import('@/images/partners/u-tech.png'),
+      website: 'https://www.magasins-u.com/'
     },
     {
       id: 'sopra-steria',
       name: 'Sopra Steria',
       image: await import('@/images/partners/sopra-steria.png'),
-      website: 'https://www.soprasteria.com'
+      website: 'https://www.soprasteria.com/'
+    },
+    {
+      id: 'capgemini',
+      name: 'Capgemini',
+      image: await import('@/images/partners/capgemini.png'),
+      website: 'https://www.capgemini.com/'
+    },
+    {
+      id: 'bouygues-telecom',
+      name: 'Bouygues Telecom',
+      image: await import('@/images/partners/bouygues-telecom.png'),
+      website: 'https://www.bouyguestelecom.fr/'
     },
     {
       id: 'thales',
-      name: 'Thales Services Numériques',
+      name: 'Thales',
       image: await import('@/images/partners/thales.png'),
-      website: 'https://www.thalesgroup.com/fr'
+      website: 'https://www.thalesgroup.com/'
     },
     {
-      id: 'u-tech',
-      name: 'U TECH',
-      image: await import('@/images/partners/u-tech.png'),
-      website: 'https://ugieiris.fr/'
+      id: 'generali',
+      name: 'Generali',
+      image: await import('@/images/partners/generali.png'),
+      website: 'https://www.generali.fr/'
     },
     {
-      id: 'zenika',
-      name: 'Zenika',
-      image: await import('@/images/partners/zenika.png'),
-      website: 'https://www.zenika.com'
+      id: 'dataiku',
+      name: 'Dataiku',
+      image: await import('@/images/partners/dataiku.png'),
+      website: 'https://www.dataiku.com/'
     }
   ],
   Gold: [
     {
-      id: 'accenture',
-      name: 'Accenture',
-      image: await import('@/images/partners/accenture.png'),
-      website: 'https://www.accenture.com'
-    },
-    {
-      id: 'blablacar',
-      name: 'Blablacar',
-      image: await import('@/images/partners/blablacar.png'),
-      website: 'https://www.blablacar.fr'
-    },
-    {
-      id: 'cat-amania',
-      name: 'CAT-Amania',
-      image: await import('@/images/partners/cat-amania.png'),
-      website: 'https://www.cat-amania.com'
-    },
-    {
-      id: 'daveo',
-      name: 'Daveo',
-      image: await import('@/images/partners/daveo.png'),
-      website: 'https://www.daveo.fr'
-    },
-    {
-      id: 'davidson',
-      name: 'Davidson',
-      image: await import('@/images/partners/davidson.png'),
-      website: 'https://www.davidson.fr'
-    },
-    {
-      id: 'delia-technologies',
-      name: 'Delia Technologies',
-      image: await import('@/images/partners/delia-technologies.png'),
-      website: 'https://www.delia.tech/'
-    },
-    {
-      id: 'doctolib',
-      name: 'Doctolib',
-      image: await import('@/images/partners/doctolib.png'),
-      website: 'https://www.doctolib.fr'
-    },
-    {
       id: 'epitech',
       name: 'Epitech',
       image: await import('@/images/partners/epitech.png'),
-      website: 'https://www.epitech.eu'
-    },
-    {
-      id: 'inetum',
-      name: 'Inetum',
-      image: await import('@/images/partners/inetum.png'),
-      website: 'https://www.inetum.com'
-    },
-    {
-      id: 'isilog',
-      name: 'Isilog',
-      image: await import('@/images/partners/isilog.jpg'),
-      website: 'https://www.isilog.com'
-    },
-    {
-      id: 'kaibee',
-      name: 'Kaïbee',
-      image: await import('@/images/partners/kaibee.png'),
-      website: 'https://www.kaibee.fr'
+      website: 'https://www.epitech.eu/'
     },
     {
       id: 'lucca',
       name: 'Lucca',
       image: await import('@/images/partners/lucca.png'),
-      website: 'https://www.lucca.fr'
+      website: 'https://www.lucca.fr/'
+    },
+    {
+      id: 'sfeir',
+      name: 'SFEIR',
+      image: await import('@/images/partners/sfeir.png'),
+      website: 'https://www.sfeir.com/'
+    },
+    {
+      id: 'daveo',
+      name: 'Daveo',
+      image: await import('@/images/partners/daveo.png'),
+      website: 'https://www.daveo.fr/'
+    },
+    {
+      id: 'mantu',
+      name: 'Amaris Consulting - Mantu',
+      image: await import('@/images/partners/mantu.png'),
+      website: 'https://www.mantu.com/'
+    },
+    {
+      id: 'accenture',
+      name: 'Accenture',
+      image: await import('@/images/partners/accenture.png'),
+      website: 'https://www.accenture.com/'
     },
     {
       id: 'neosoft',
@@ -162,52 +114,64 @@ export const partnersByTypes: Record<PartnerType, Partner[]> = {
       website: 'https://www.neosoft.fr/'
     },
     {
-      id: 'orange-business',
-      name: 'Orange Business',
-      image: await import('@/images/partners/orange-business.png'),
-      website: 'https://www.orange-business.com'
-    },
-    {
-      id: 'ovh',
-      name: 'OVH',
-      image: await import('@/images/partners/ovh.png'),
-      website: 'https://www.ovh.com'
-    },
-    {
-      id: 'rca',
-      name: 'RCA',
-      image: await import('@/images/partners/rca.png'),
-      website: 'https://www.rca.fr'
+      id: 'sii',
+      name: 'SII',
+      image: await import('@/images/partners/sii.png'),
+      website: 'https://www.sii.fr/'
     },
     {
       id: 'shopopop',
       name: 'Shopopop',
       image: await import('@/images/partners/shopopop.png'),
-      website: 'https://www.shopopop.com'
+      website: 'https://www.shopopop.com/'
     },
     {
-      id: 'sii',
-      name: 'SII',
-      image: await import('@/images/partners/sii.png'),
-      website: 'https://sii-group.com/fr-FR'
+      id: 'objectware',
+      name: 'Objectware',
+      image: await import('@/images/partners/objectware.svg'),
+      website: 'https://www.objectware.fr/'
+    },
+    {
+      id: 'maif',
+      name: 'MAIF',
+      image: await import('@/images/partners/maif.png'),
+      website: 'https://www.maif.fr/'
+    },
+    {
+      id: 'orange-business',
+      name: 'Orange Business',
+      image: await import('@/images/partners/orange-business.png'),
+      website: 'https://www.orange-business.com/'
+    },
+    {
+      id: 'zenika',
+      name: 'Zenika',
+      image: await import('@/images/partners/zenika.png'),
+      website: 'https://www.zenika.com/'
+    },
+    {
+      id: 'bpce-si',
+      name: 'BPCE SI',
+      image: await import('@/images/partners/bpce-si.png'),
+      website: 'https://www.bpce-si.fr/'
     },
     {
       id: 'sncf-connect-tech',
       name: 'SNCF Connect & Tech',
       image: await import('@/images/partners/sncf-connect-tech.png'),
-      website: 'https://www.sncf-connect-tech.fr/'
+      website: 'https://www.sncf-connect.com/'
     },
     {
-      id: 'vif',
-      name: 'VIF',
-      image: await import('@/images/partners/vif.png'),
-      website: 'https://www.vif.fr'
+      id: 'qlik',
+      name: 'Qlik (Talend SAS)',
+      image: await import('@/images/partners/qlik.svg'),
+      website: 'https://www.qlik.com/'
     },
     {
-      id: 'younup',
-      name: 'Younup',
-      image: await import('@/images/partners/younup.png'),
-      website: 'https://www.younup.fr/'
+      id: 'catamania',
+      name: 'Catamania',
+      image: await import('@/images/partners/catamania.png'),
+      website: 'https://www.catamania.com/'
     }
   ],
   Virtual: [
@@ -215,31 +179,7 @@ export const partnersByTypes: Record<PartnerType, Partner[]> = {
       id: 'sully-group',
       name: 'Sully Group',
       image: await import('@/images/partners/sully-group.png'),
-      website: 'https://www.sully-group.com'
-    },
-    {
-      id: 'valeuriad',
-      name: 'Valeuriad',
-      image: await import('@/images/partners/valeuriad.png'),
-      website: 'https://www.valeuriad.com'
-    },
-    {
-      id: 'modalb',
-      name: 'ModalB',
-      image: await import('@/images/partners/modalb.webp'),
-      website: 'https://www.modalbgroup.com/'
-    },
-    {
-      id: 'ab-tasty',
-      name: 'AB Tasty',
-      image: await import('@/images/partners/ab-tasty.png'),
-      website: 'https://www.abtasty.com/fr/'
-    },
-    {
-      id: 'malt',
-      name: 'Malt',
-      image: await import('@/images/partners/malt.webp'),
-      website: 'https://www.malt.com'
+      website: 'https://www.sully-group.com/'
     }
   ],
   Velotypie: [
